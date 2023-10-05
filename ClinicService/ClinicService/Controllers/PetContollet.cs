978f76f3dbe3d8b88d9f72cd2928b645c70665a7
aleksey <arxangel1310@mail.ru>
2023-10-05 16:20:15 +0900

    first commit

---
+++ ClinicService/ClinicService/Controllers/PetContollet.cs
@@ -0,0 +1,67 @@
+﻿using ClinicService.Models.Requests;
+using ClinicService.Models;
+using ClinicService.Services;
+using ClinicService.Services.Impl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.PortableExecutable;
+
+namespace ClinicService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+
+        private readonly IPetRepository _petRepository;
+
+        public PetController(IPetRepository petRepository)
+        {
+            _petRepository = petRepository;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] CreatePetRequest createRequest)
+        {
+            int res = _petRepository.Create(new Pet
+            {
+                ClientId = createRequest.ClientId,
+                Name = createRequest.Name,
+                Birthday = createRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] UpdatePetRequest updateRequest)
+        {
+            int res = _petRepository.Update(new Pet
+            {
+                ClientId = updateRequest.ClientId,
+                Name = updateRequest.Name,
+                Birthday = updateRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] int PetId)
+        {
+            int res = _petRepository.Delete(PetId);
+            return Ok(res);
+        }
+
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            return Ok(_petRepository.GetAll());
+        }
+
+
+        [HttpGet("get/{petId}")]
+        public IActionResult GetById([FromRoute] int PetId)
+        {
+            return Ok(_petRepository.GetById(PetId));
+        }
+    }
+}
